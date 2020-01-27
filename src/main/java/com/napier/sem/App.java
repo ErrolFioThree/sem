b671c97a358dce058627d3fa3b24dddc6aca42c4
40398647 <40398647@live.napier.ac.uk>
2020-01-27 11:36:53 +0000

    Adding a real project now :)

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("did you ever hear the tragedy of darth plagueis the wise?");
+    }
+}
